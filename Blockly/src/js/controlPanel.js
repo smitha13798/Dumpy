8473290d2926aff19b7771369510a3b64775145d
Sushmitha <52081487+smitha13798@users.noreply.github.com>
2024-09-23 14:01:49 +0530

    Delete Blockly directory

--- Blockly/src/js/controlPanel.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('runButton').addEventListener('click', () => {
-    // Example of running code
-    alert('Running the code!');
-});
-
-// Additional handlers for saving, loading, exporting, etc.
